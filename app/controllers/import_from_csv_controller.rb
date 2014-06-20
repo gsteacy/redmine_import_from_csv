e93f1c15ec87138f371ad9fbeb80066e5ebcb8d2
Gavin Steacy <gavin.steacy@quantium.com.au>
2014-06-20 23:51:36 +1000

    Allow importing more fields
    Column order is no longer important. I've removed some checks for duplicate issues but might include something similar again later. Right now the user must remove any lines from the file which import successfully if there are errors.

--- app/controllers/import_from_csv_controller.rb
+++ app/controllers/import_from_csv_controller.rb
@@ -1,81 +1,139 @@
 class ImportFromCsvController < ApplicationController
-  before_filter :get_project,:authorize
+  before_filter :get_project, :authorize
   helper :import_from_csv
   include ImportFromCsvHelper
   include ApplicationHelper
-  
-  #menu_item :import_from_csv
+
   require 'csv'
+
   def index
     respond_to do |format|
       format.html
     end
   end
-  
+
   def csv_import
     if params[:dump][:file].blank?
       error = 'Please, Select CSV file'
       redirect_with_error error, @project
     elsif params[:dump][:daily_working_hrs].blank?
-      error = 'Please, Enter Expected daily working hours'
+      error = 'Please enter Expected daily working hours'
       redirect_with_error error, @project
     else
       begin
-        done = 0;total = 0
+        done = 0; total = 0
         error_messages = []
-        tracker = @project.trackers.find(params[:dump][:tracker_id])
         infile = params[:dump][:file].read
         parsed_file = CSV.parse(infile)
-        parsed_file.each_with_index  do |row,index|
-          story_id=row[0];subject=row[1]
-          description=row[2];estimated_hrs=row[3]
-          next if index == 0
+
+        if parsed_file.none?
+          redirect_with_error('CSV is empty', @project)
+          return
+        end
+
+        headings = map_headings parsed_file.shift
+        mandatory_headings = [:author, :subject, :tracker]
+        missing_headings = headings.select { |k, v| v.nil? and mandatory_headings.include? k }.keys
+
+        if missing_headings.any?
+          redirect_with_error("Missing mandatory headings: #{missing_headings.map { |h| h.capitalize }.join ', '}", @project)
+          return
+        end
+
+        parsed_file.each_with_index do |row, index|
+          invalid = false
           total = total+1
+
           issue = Issue.new
           issue.project = @project
-          issue.author = User.current
-          issue.tracker = tracker
-          issue.text_id = story_id if issue.has_attribute? 'text_id'
-          issue.subject = subject
-          issue.description = description
-          issue.estimated_hours=estimated_hrs.to_f * params[:dump][:daily_working_hrs].to_f unless estimated_hrs.blank? and params[:dump][:daily_working_hrs].blank?
-          if issue.save
-            done=done+1
-          else # invalid
-            if issue.has_attribute? 'text_id'
-              duplicate_issue = Issue.all(:conditions => "text_id = BINARY '#{story_id}' AND project_id = #{@project.id}")
-              if duplicate_issue.blank?
-                error_messages << "Line:#{index+1}..Error: #{issue.errors.full_messages.uniq.join(', ')}"
-              else
-                i_id = duplicate_issue.first.id
-                error_messages << "Line:#{index+1}..Error: #{issue.errors.full_messages.uniq.join(', ')} for this issue <a href=\"/issues/#{i_id}\">##{i_id}</a>"
-              end 
+
+          author = row[headings[:author]]
+          issue.author = get_user author
+
+          if issue.author.nil?
+            error_messages << "Line #{index+1}: User '#{author}' is not a member of the project"
+            invalid = true
+          end
+
+          tracker = row[headings[:tracker]]
+          issue.tracker = @project.trackers.find_by_name tracker
+
+          if issue.tracker.nil?
+            error_messages << "Line #{index+1}: Tracker '#{tracker}' is invalid or not assigned to this project"
+            invalid = true
+          end
+
+          issue.subject = row[headings[:subject]]
+          issue.description = row[headings[:description]]
+
+          status = row[headings[:status]]
+          issue.status = IssueStatus.find_by_name status
+
+          if issue.status.nil?
+            if status.blank?
+              issue.status = IssueStatus.default
             else
-              error_messages << "Line:#{index+1}..Error: #{issue.errors.full_messages.uniq.join(', ')}"
-            end         
+              error_messages << "Line #{index+1}: Status '#{status}' is invalid"
+              invalid = true
+            end
+          end
+
+          assignee = row[headings[:assignee]]
+          issue.assigned_to = get_user assignee unless assignee.nil?
+
+          if issue.assigned_to.nil? and !assignee.nil?
+            error_messages << "Line #{index+1}: User '#{assignee}' is not a member of the project"
+            invalid = true
+          end
+
+          issue.estimated_hours=row[headings[:estimated_hours]].to_f
+
+          unless invalid
+            if issue.save
+              done = done+1
+            else
+              error_messages << "Line #{index+1}: #{issue.errors.full_messages.uniq.join(', ')}"
+            end
           end
         end
-      rescue CSV::MalformedCSVError => e
-        redirect_with_error e.message, @project
-        return
       end
       if done == total
         flash[:notice]="CSV Import Successful, #{done} new issues have been created"
       else
-        flash[:error]=format_error(done,total,error_messages)
+        flash[:error]=format_error(done, total, error_messages)
       end
-      redirect_to :controller=>"issues",:action=>"index",:project_id=>@project.identifier
+      redirect_to :controller => "issues", :action => "index", :project_id => @project.identifier
     end
   end
-  
-  def redirect_with_error(err,project)
+
+  def redirect_with_error(err, project)
     flash[:error]=err
-    redirect_to :controller=>"import_from_csv",:action=>"index",:project_id=>project.identifier
+    redirect_to :controller => "import_from_csv", :action => "index", :project_id => project.identifier
   end
- 
+
   def get_project
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
+  end
+
+  def get_user(username)
+    member = @project.members.joins(:user).where('users.login' => username).first
+    member.user unless member.nil?
+  end
+
+  def map_headings(heading_row)
+    heading_row = heading_row.map { |h| h.downcase }
+    headings = {}
+
+    headings[:subject] = heading_row.index('subject')
+    headings[:description] = heading_row.index('description')
+    headings[:status] = heading_row.index('status')
+    headings[:tracker] = heading_row.index('tracker')
+    headings[:author] = heading_row.index('author')
+    headings[:assignee] = heading_row.index('assignee')
+    headings[:estimated_hours] = heading_row.index('estimated hours')
+
+    headings
   end
 end
