58fd380084566b4921d015d4107f77ffd6583709
Gavin Steacy <gavin.steacy@quantium.com.au>
2014-06-20 23:57:04 +1000

    Removed note about required format
    Slightly reformatted code

--- app/helpers/import_from_csv_helper.rb
+++ app/helpers/import_from_csv_helper.rb
@@ -1,20 +1,17 @@
 module ImportFromCsvHelper
-  def format_error(done,total,error_messages)
-    final_message=""
+  def format_error(done, total, error_messages)
     final_message=%{#{done} out of #{total} new issues have been created}
-    final_message << %{<p>Details:</p>}
-    final_message << %{<ul>}
+    final_message << '<p>Details:</p>'
+    final_message << '<ul>'
     for message in error_messages
       final_message << %{<li>#{message}</li>}
     end
-    final_message << %{</ul>}
-    final_message << %{<p>#{l(:required_format)}</p>}
-    return final_message
+    final_message << '</ul>'
+    final_message
   end
 
   def has_story_tracker?(project)
-    found=project.trackers.find_by_name("Story")
-    return found.id  if found
+    found=project.trackers.find_by_name('Story')
+    found.id if found
   end
-
 end
